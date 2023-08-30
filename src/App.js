9206cb05007aee9b9d79c3056f567478508f2a89
DE101 <dinganaearnestfohtung@gmail.com>
2023-08-30 19:17:27 +0100

    added content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      <Signup></Signup>
+      <Signup />
     </div>
     
   )
--- src/Sign up.js
+++ src/Sign up.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 
-
 export default function Signup() {
-    const [name, setCount] = React.useState(0);
+    const [name, setName] = React.useState(0);
     const [email, setEmail] = React.useState(0);
 
     function updateCount(elem) {
-        setCount(elem.target.value);
+        setName(elem.target.value);
     }
     function updateEmail(elem) {
         setEmail(elem.target.value);
@@ -16,14 +15,30 @@ export default function Signup() {
         event.preventDefault();
         const payLoad = {name, email};
         console.log(payLoad);
+
+        fetch ("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            body: JSON.stringify(payLoad),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+          .then((response) => response.json())
+          .then((json) => console.log(json));
+
     }
+
+
+
     return (
         <form onSubmit={submitForm} action="./signup.php">
             <div className="control">
-                <label>Name:</label> <input required onChange={updateCount()} type="text"/>
+                <label>Name:</label> {" "}
+                <input required onChange={updateCount} type="text"/>
             </div>
             <div className="control">
-                <label>Email:</label> <input required onChange={updateEmail()} type="email"/>
+                <label>Email:</label> {" "}
+                <input required onChange={updateEmail} type="email"/>
             </div>
             <div className="control">
                 <input type="submit"/>
